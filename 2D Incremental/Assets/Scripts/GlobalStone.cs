bce715145b7c969772ce4c0a71631d2ddbe128a2
Louis P <LJPayne98@gmail.com>
2018-08-18 13:14:58 +0100

    Added Stone and Wood resources

---
+++ 2D Incremental/Assets/Scripts/GlobalStone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalStone : MonoBehaviour {
+
+    public static int StoneCount;
+    public GameObject StoneDisplay;
+    public int InternalStone;
+
+    // Update is called once per frame
+    void Update()
+    {
+        InternalStone = StoneCount;
+        StoneDisplay.GetComponent<Text>().text = "Wood:" + InternalStone;
+    }
+}
+
