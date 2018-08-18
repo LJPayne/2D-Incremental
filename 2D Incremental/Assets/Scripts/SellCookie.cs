ab55ba9924a088d3167de12f49cae22acd562ee0
Edouard Payne <ejpayne@hotmail.co.uk>
2018-08-18 12:34:11 +0100

    Added Sell Button

---
+++ 2D Incremental/Assets/Scripts/SellCookie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellCookie : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public void ClickThatButton() {
+        GlobalCookies.CookieCount -= 1;
+        GlobalCash.CashCount += 1;
+    }
+}
